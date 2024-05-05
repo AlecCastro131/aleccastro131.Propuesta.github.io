acf8c6d0bdd3da109fe6a8b5b74179a593bebea8
Alec Catro <aleccastro131@gmail.com>
2024-05-04 18:35:48 -0600

    Add files via upload

---
+++ Propuesta.js
@@ -0,0 +1,4 @@
+document.getElementById('answer-button').addEventListener('click', function() {
+    var answer = document.getElementById('answer-input').value;
+    console.log('Respuesta: ' + answer);
+});
